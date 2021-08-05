919ef9a4a14ca43eadae0c4fd75b227601cbb26a
Luis Garrido <luis.garrido@sisgarbe.pt>
2021-08-05 12:07:13 +0100

    Kendo Material ripple effect package installed: ng add @progress/kendo-angular-ripple

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GridModule } from '@progress/kendo-angular-grid';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RippleModule } from '@progress/kendo-angular-ripple';
+
 
 
 
@@ -14,7 +16,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     GridModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
